79034abda067698be570038d20ea120d1f13dcc7
David Doria <daviddoria@gmail.com>
2011-07-21 10:12:40 -0400

    First commit

---
+++ PCLtoVTK.cxx
@@ -0,0 +1,58 @@
+// STL
+#include <iostream>
+
+// PCL
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+// VTK
+#include <vtkPolyData.h>
+#include <vtkPoints.h>
+#include <vtkXMLPolyDataWriter.h>
+
+int main (int argc, char*argv[])
+{
+  // Verify arguments
+  if(argc < 3)
+    {
+    std::cerr << "Required arguments: input.pcd output.vtp" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // Parse arguments
+  std::string inputFileName = argv[1];
+  std::string outputFileName = argv[2];
+  
+  // Output arguments
+  std::cout << "Reading " << inputFileName << " and writing " << outputFileName << std::endl;
+  
+  // Read the PCD file
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> (inputFileName.c_str(), *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file \n");
+    return EXIT_FAILURE;
+  }
+
+  // Construct the VTK data
+  vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+    {
+    points->InsertNextPoint ( cloud->points[i].x, cloud->points[i].y, cloud->points[i].z );
+    }
+ 
+  // Create a polydata object and add the points to it.
+  vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
+  polydata->SetPoints(points);
+ 
+  // Write the file
+  vtkSmartPointer<vtkXMLPolyDataWriter> writer =  
+    vtkSmartPointer<vtkXMLPolyDataWriter>::New();
+  writer->SetFileName("test.vtp");
+  writer->SetInputConnection(polydata->GetProducerPort());
+  writer->Write();
+  
+  return EXIT_SUCCESS;
+}
---
+++ VTKtoPCL.cxx
@@ -0,0 +1,54 @@
+// STL
+#include <iostream>
+
+// PCL
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+// VTK
+#include <vtkPolyData.h>
+#include <vtkPoints.h>
+#include <vtkXMLPolyDataReader.h>
+
+int main (int argc, char** argv)
+{
+  // Verify arguments
+  if(argc < 3)
+    {
+    std::cerr << "Required arguments: input.vtp output.pcd" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // Parse arguments
+  std::string inputFileName = argv[1];
+  std::string outputFileName = argv[2];
+  
+  // Output arguments
+  std::cout << "Reading " << inputFileName << " and writing " << outputFileName << std::endl;
+  
+  // Read the VTP file
+  vtkSmartPointer<vtkXMLPolyDataReader> reader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+  reader->SetFileName(inputFileName.c_str());
+  reader->Update();
+  
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+
+  // Fill in the cloud data
+  cloud.width    = reader->GetOutput()->GetNumberOfPoints();
+  cloud.height   = 1; // This indicates that the point cloud is unorganized
+  cloud.is_dense = false;
+  cloud.points.resize (cloud.width);
+
+  for (size_t i = 0; i < cloud.points.size (); ++i)
+  {
+    double p[3];
+    reader->GetOutput()->GetPoint(i,p);
+    cloud.points[i].x = p[0];
+    cloud.points[i].y = p[1];
+    cloud.points[i].z = p[2];
+  }
+
+  pcl::io::savePCDFileASCII (outputFileName.c_str(), cloud);
+
+  return EXIT_SUCCESS;
+}
