aff67eea5ab9ba64450b7cbf445567eeb2c59862
Jaudiel Vélez <jaudiel.velez@upr.edu>
2011-07-27 15:57:37 -0400

    Added support for converting multiple files simultaneously

--- PCLtoVTKExample.cxx
+++ PCLtoVTKExample.cxx
@@ -17,40 +17,44 @@
 int main (int argc, char*argv[])
 {
   // Verify arguments
-  if(argc < 3)
+  if(argc < 2)
     {
-    std::cerr << "Required arguments: input.pcd output.vtp" << std::endl;
+    std::cerr << "Required arguments: input.pcd [input2.pcd input3.pcd ... inputN.pcd]" << std::endl;
     return EXIT_FAILURE;
     }
-
-  // Parse arguments
-  std::string inputFileName = argv[1];
-  std::string outputFileName = argv[2];
-  
-  // Output arguments
-  std::cout << "Reading " << inputFileName << " and writing " << outputFileName << std::endl;
-  
-  // Read the PCD file
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  std::cout << pcl::getFieldsList<pcl::PointXYZ>(*cloud);
-
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> (inputFileName.c_str(), *cloud) == -1) //* load the file
-  {
-    PCL_ERROR ("Couldn't read file \n");
-    return EXIT_FAILURE;
-  }
-
-  // Create a polydata object and add the points to it.
-  vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
-
-  PCLtoVTK<pcl::PointXYZ>(cloud, polydata);
  
-  // Write the file
-  vtkSmartPointer<vtkXMLPolyDataWriter> writer =
+  std::vector<std::string> input_files;
+
+  // Parse and output arguments
+  for (int i = 1; i < argc; ++i) {
+    std::cout << "Reading " << argv[i] << " and writing " << argv[i] << ".vtp" << std::endl;
+  
+    // Read the PCD file
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    std::cout << pcl::getFieldsList<pcl::PointXYZ>(*cloud) << std::endl;
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[i], *cloud) == -1) //* load the file
+    {
+      PCL_ERROR ("Couldn't read file \n");
+      return EXIT_FAILURE;
+    }
+
+    // Create a polydata object and add the points to it.
+    vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
+
+    PCLtoVTK<pcl::PointXYZ>(cloud, polydata);
+ 
+    // Write the file
+    vtkSmartPointer<vtkXMLPolyDataWriter> writer =
     vtkSmartPointer<vtkXMLPolyDataWriter>::New();
-  writer->SetFileName(outputFileName.c_str());
-  writer->SetInputConnection(polydata->GetProducerPort());
-  writer->Write();
+
+    std::string outputFileName = argv[i];
+    outputFileName += ".vtp";
+
+    writer->SetFileName(outputFileName.c_str());
+    writer->SetInputConnection(polydata->GetProducerPort());
+    writer->Write();
+  }
   
   return EXIT_SUCCESS;
 }
